6cd7a3d7fb82bd7be86901d3b3a996ec8089aa2f
Shubhankar <shubhankarinvisible@gmail.com>
2024-11-15 19:46:18 +0530

    add catch all route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Header } from "./components/Common";
 import { Home, Dashboard, CoinPage, WatchList } from "./pages";
 import { ThemeProvider } from "@emotion/react";
